95b016f3fb422d791fd46060a475dae03279ffe4
create-alt <134464021+create-alt@users.noreply.github.com>
2023-06-10 00:09:50 +0900

    Update calculate_parent.js

--- calculate_parent.js
+++ calculate_parent.js
@@ -11,29 +11,5 @@ export class myfunc{
       this.test=document.getElementById(pointSmall);
       this.prints=document.getElementsByName(radio1);
       this.presentation=document.getElementsByName(radio2);
-   
-      this.sumpoint=this.exampoint_a+this.test_a+this.print_point+this.presentation_point;
-      console.log(this.sumpoint);
-      
-      if(this.sumpoint>=60){
-      
-      //pにid=finalを代入
-      let p=document.getElementById('finalJ');
-   
-      p.textContent ='合格です';
-
-      }else if(this.sumpoint<60){
-      
-      let p = document.getElementById('finalJ');
-      
-      p.textContent = '不合格です';
-
-      }else{
-
-      let p = document.getElementById('finalJ');
-      
-      p.textContent = 'error,値が正しく入力されていない可能性があります。';
-    
-    }
    }
 }
